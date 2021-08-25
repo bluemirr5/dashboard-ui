606c3cf2f666fa14117f180285a7053620d20ac7
gs won <black@parastar.co.kr>
2021-08-25 22:19:14 +0900

    20210825

--- src/views/project/ProjectReg.vue
+++ src/views/project/ProjectReg.vue
@@ -9,49 +9,49 @@
 	regDate: Date,
 	modDate: Date
     -->
-    <el-form :model="form" label-width="120px">
-      <el-form-item label="프로젝트 명">
-      <el-input v-model="form.name"></el-input>
+    <el-form :model="form" ref="totalForm" :rules="form.rules" label-width="120px">
+      <el-form-item label="프로젝트 명" prop="name">
+        <el-input v-model="form.name"></el-input>
       </el-form-item>
-      <el-form-item label="내용">
+      <el-form-item label="내용" prop="desc">
         <el-input type="textarea" v-model="form.desc"></el-input>
       </el-form-item>
 
-      <el-form-item label="상태">
+      <el-form-item label="상태" prop="status">
         <el-select v-model="form.status" placeholder="please select your zone">
           <el-option label="Zone one" value="shanghai"></el-option>
           <el-option label="Zone two" value="beijing"></el-option>
         </el-select>
       </el-form-item>
 
-      <el-form-item label="이미지 url">
+      <el-form-item label="이미지 url" prop="img">
         <el-input v-model="form.img"></el-input>
       </el-form-item>
 
-      <el-form-item label="참가자">
-        <div v-for="temp in form.user" :key="temp.id">
-          <el-checkbox v-model="temp.checked" :label="temp.name + ' - ' + temp.nickName" ></el-checkbox>
-        </div>
-
+      <el-form-item  label="참가자" prop="selectedMember">
         <el-autocomplete
-          v-model="state2"
+          v-model="form.searchMember"
           class="inline-input"
           placeholder="검색어를 입력하세요"
           :trigger-on-focus="false"
           :fetch-suggestions="querySearch"
           @select="handleSelect"
-        ></el-autocomplete>
+        >
+          <template #default="{ item }">
+            <div class="value">{{ item.value }}</div>
+          </template>
+        </el-autocomplete>
 
         <div>
-          <p v-for="item in selectedItems">
-            <span>{{item}}</span>
-          </p>
+          <el-tag v-for="item in form.selectedMember" :key="item" closable @close="handleClose(item)" >
+            {{item}}
+          </el-tag>
         </div>
 
       </el-form-item>
 
       <el-form-item>
-        <el-button type="primary" @click="textLog">Create</el-button>
+        <el-button type="primary" @click="create">Create</el-button>
         <el-button>Cancel</el-button>
       </el-form-item>
     </el-form>
@@ -59,67 +59,74 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, onMounted, ref} from "vue";
+import { defineComponent, reactive, ref } from "vue";
+import { FieldErrorList } from "async-validator";
+import { ElForm } from "element-plus";
 
 export default defineComponent({
   name: "ProjectReg",
   setup() {
     const form = reactive({
-      name: '',
-      status: '',
-      img: '',
-      desc: '',
-      user: [{id: 1, name: 'test@test.com', nickName: 'test', checked: false},
-        {id: 2, name: 'test2@test.com', nickName: 'test3', checked: false},
-        {id: 3, name: 'test2@test.com', nickName: 'test3', checked: false}],
-      value: []
-    })
+      name: "",
+      status: "",
+      img: "",
+      desc: "",
+      searchMember: "",
+      selectedMember: new Array<string>(),
+      rules: {
+        name: [ {required: true, message: '프로젝트 이름을 넣어주세요', trigger: 'blur'} ],
+        desc: [ {message: '프로젝트 설명을 입력해주세요', trigger: 'blur'} ],
+        status: [ {required: true, message: '프로젝트의 상태를 입력해주세요', trigger: 'blur'} ],
+        img: [ {message: '프로젝트 관련 이미지를 넣어주세요', trigger: 'blur'} ],
+        selectedMember: [ {type: 'array', required: true, min: 1, message: '1명 이상 멤버를 선택해주세요'} ]
+      }
+    });
 
-    const textLog = () => {
-      window.console.log(form)
-    }
+    interface Callback { (isValid?: boolean, invalidFields?: FieldErrorList): void; }
+    type MElForm = { validate: (callback?: Callback) => Promise<boolean> }
 
-
-    //
-
-    const state2 = ref('')
-
-    type Temp = {
-      value: string,
-      link: string
-    }
-    const selectedItems:string[] = reactive([]);
-    const querySearch = (queryString: String, cb: (result: Temp[]) => void) => {
-      window.console.log(queryString) //
+    const totalForm = ref<MElForm>();
+    const create = () => {
+      totalForm.value?.validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    };
+    const querySearch = (queryString: string, cb: (result: {value: string}[]) => void) => {
       // if(queryString.length > 3) {
-      //   this.$http.get('api/searchKey?keyword=' + queryString).then(resp => {
-      //
-      //   })
+        // axios.get('/api/getUser?keyword='+queryString).then(resp => {
+        //   if(resp === 200) {
+        //     cb(resp.data);
+        //   }
+        // }, err => {window.console.error(err.response)})
       // }
-
-      if(queryString === 's')  {
-        cb([{value:'s1', link: 'bb'}, {value:'s2', link: 'aa'}])
-      } else if(queryString === 'a') {
-        cb([{value:'a1', link: 'bb'}, {value:'a2', link: 'aa'}])
+      if (queryString === "s") {
+        cb([{ value: "s1" }, { value: "s2" }]);
+      } else if (queryString === "a") {
+        cb([{ value: "a1" }, { value: "a2" }]);
       } else {
-        cb([])
+        cb([]);
       }
     };
-    const handleSelect = (selected: Temp)=> {
-      selectedItems.push(selected.value)
-      state2.value = ''
-    }
+    const handleSelect = (selected: {value: string}) => {
+      form.selectedMember.push(selected.value);
+      form.searchMember = "";
+    };
+    const handleClose = (tag: string) => form.selectedMember.splice(form.selectedMember.indexOf(tag), 1);
 
     return {
       form,
-      textLog,
-      //
-      state2,
+      create,
       querySearch,
       handleSelect,
-      selectedItems
-    }
-  }
+      handleClose,
+      totalForm
+    };
+  },
 });
 </script>
 
